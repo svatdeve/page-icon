a7e06541f5532b856ba690ef5f08f4a785f468df
Jia Hao <jiahaog@gmail.com>
2016-03-04 00:23:21 +0800

    Add source maps support

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
+import 'source-map-support/register';
+
 const getPage = require('./modules/getPage');
 const getIconLinks = require('./modules/getIconLinks');
 const downloadIcons = require('./modules/downloadIcons');
