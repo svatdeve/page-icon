bf084f87f56fcbb407f37f9dd5c7e9635bacfc24
Jia Hao <jiahaog@gmail.com>
2016-03-04 00:26:00 +0800

    Check for open graph images as well

--- src/modules/getIconLinks.js
+++ src/modules/getIconLinks.js
@@ -11,17 +11,41 @@ function getDomainUrl(someUrl) {
     return parsedUrl.format()
 }
 
-function getIconLinks(rootUrl, dom) {
-    var $ = cheerio.load(dom);
-    const iconLinks = [];
+function linkTagLinks($, rootUrl) {
+    const links = [];
     $('link').each(function(index, element) {
         const href = $(element).attr('href');
         const resolved = url.resolve(rootUrl, href);
         if (!hrefIsIcon(resolved)) {
             return;
         }
-        iconLinks.push(resolved);
+        links.push(resolved);
     });
+    return links;
+}
+
+function metaTagLinks($, rootUrl) {
+    const links = [];
+    $('meta').each((index, element) => {
+        const property = $(element).attr('property');
+        if (property !== 'og:image') {
+            return;
+        }
+
+        const graphImageUrl = $(element).attr('content');
+        const resolved = url.resolve(rootUrl, graphImageUrl);
+        links.push(resolved);
+    });
+
+    return links
+}
+
+function getIconLinks(rootUrl, dom) {
+    var $ = cheerio.load(dom);
+    let iconLinks = [];
+
+    iconLinks = iconLinks.concat(linkTagLinks($, rootUrl));
+    iconLinks = iconLinks.concat(metaTagLinks($, rootUrl));
 
     iconLinks.push(url.resolve(getDomainUrl(rootUrl), 'apple-touch-icon.png'));
     return iconLinks;
