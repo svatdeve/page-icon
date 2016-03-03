410222f5120844ddc47e7591c2ac08698df1929a
Jia Hao <jiahaog@gmail.com>
2016-03-03 21:07:06 +0800

    Return icon path with largest size

--- src/modules/findBestIcon.js
+++ src/modules/findBestIcon.js
@@ -1,6 +1,42 @@
+const fs = require('fs');
+
+function getFileSize(filePath) {
+    return new Promise((resolve, reject) => {
+        fs.stat(filePath, function(error, fileInfo) {
+            if (error) {
+                reject(error);
+                return;
+            }
+
+            const fileSizeKB = fileInfo.size / 1000;
+            resolve({
+                sizeKB: fileSizeKB,
+                path: filePath
+            });
+        });
+    });
+}
+
+function getFileSizes(filePaths) {
+    const getFileSizePromises = filePaths.map(getFileSize);
+    return Promise.all(getFileSizePromises)
+}
+
+function sortFileBySizes(fileSizes) {
+    return fileSizes.sort(function(a, b){
+        if (a.sizeKB < b.sizeKB) {
+            return 1;
+        } else {
+            return -1;
+        }
+    });
+}
+
 function findBestIcon(iconPaths) {
-    return new Promise(function (resolve, reject) {
-        resolve(iconPaths[0]);
+    return getFileSizes(iconPaths).then(fileSizes => {
+        // sort descending
+        const sortedFileSizes = sortFileBySizes(fileSizes);
+        return sortedFileSizes[0].path;
     });
 }
 
