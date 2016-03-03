e0efb763236210c922622307377928ec595a1fe0
Jia Hao <jiahaog@gmail.com>
2016-03-03 18:06:05 +0800

    Use temporary directory for icons

--- src/modules/downloadIcons.js
+++ src/modules/downloadIcons.js
@@ -1,22 +1,39 @@
 const axios = require('axios');
 const fs = require('fs');
 const path = require('path');
+const tmp = require('tmp');
 
-function saveToFile(data) {
-    return new Promise(function(resolve, reject) {
-        const saveDir = path.join(__dirname, 'someicon.ico');
-        fs.writeFile(saveDir, data, function (error) {
+tmp.setGracefulCleanup();
+
+function getTempDir() {
+    return new Promise((resolve, reject) => {
+        tmp.dir((error, dirPath) => {
             if (error) {
                 reject(error);
                 return;
             }
-            resolve(saveDir);
+            resolve(dirPath);
         });
     });
 }
 
-function downloadIcon(iconUrl) {
+function saveToFile(data) {
+    return getTempDir()
+        .then(dirPath => {
+            return new Promise(function(resolve, reject) {
+                const saveDir = path.join(dirPath, 'someicon.ico');
+                fs.writeFile(saveDir, data, function (error) {
+                    if (error) {
+                        reject(error);
+                        return;
+                    }
+                    resolve(saveDir);
+                });
+            });
+        });
+}
 
+function downloadIcon(iconUrl) {
     const iconData = new Promise(function (resolve, reject) {
         axios.get(iconUrl, {responseType: 'arraybuffer'})
             .then(function(response) {
