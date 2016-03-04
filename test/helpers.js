48a184789b0eb6637cf5bcfcd32059efe9bbd897
Jia Hao <jiahaog@gmail.com>
2016-03-04 13:00:02 +0800

    Shift helpers to separate file

---
+++ test/helpers.js
@@ -0,0 +1,27 @@
+const fs = require('fs');
+const path = require('path');
+
+const ICON_SIZE_THRESHOLD_KB = 0.5;
+const ICON_DIR = path.join(__dirname, '..', 'out');
+
+function isIconValid(icon) {
+    const fileSizeKB = icon.data.length / 1000;
+    return fileSizeKB > ICON_SIZE_THRESHOLD_KB;
+}
+
+function saveToFile(icon) {
+    const filePath = path.join(ICON_DIR, `${icon.name}.${icon.ext}`);
+    return new Promise(function(resolve, reject) {
+        fs.writeFile(filePath, icon.data, function(error) {
+            if (error) {
+                reject(error);
+                return;
+            }
+            resolve(filePath);
+        });
+    });
+}
+
+module.exports = {
+    isIconValid, saveToFile
+};
--- test/test.js
+++ test/test.js
@@ -1,41 +1,23 @@
 const pageIcon = require('./../lib/index');
-const chai = require('chai');
+const helpers = require('./helpers');
 const fs = require('fs');
+const chai = require('chai');
 const path = require('path');
 const url = require('url');
+
 const expect = chai.expect;
-const ICON_SIZE_THRESHOLD_KB = 0.5;
+const {isIconValid, saveToFile} = helpers;
 
 const SITE_URLS = [
     'https://www.facebook.com/',
-    'http://stackoverflow.com/questions/16301503/can-i-use-requirepath-join-to-safely-concatenate-urls'
+    'http://stackoverflow.com/questions/16301503/can-i-use-requirepath-join-to-safely-concatenate-urls',
+    'https://web.whatsapp.com'
 ];
-
-const ICON_DIR = path.join(__dirname, '..', 'out');
-
-function isIconValid(icon) {
-    const fileSizeKB = icon.data.length / 1000;
-    return fileSizeKB > ICON_SIZE_THRESHOLD_KB;
-}
-
-function saveToFile(icon) {
-    const filePath = path.join(ICON_DIR, `${icon.source}${icon.type}`);
-    return new Promise(function(resolve, reject) {
-        fs.writeFile(filePath, icon.data, function(error) {
-            if (error) {
-                reject(error);
-                return;
-            }
-            resolve(filePath);
-        });
-    });
-}
 
 describe('Page Icon', function() {
     this.timeout(10000);
 
     it('Can download all icons', function(done) {
-
         const downloadTests = SITE_URLS.map(function(siteUrl) {
             return new Promise(function(resolve, reject) {
                 pageIcon(siteUrl)
